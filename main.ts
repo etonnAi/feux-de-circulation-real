95e5b7030df6fd2defa6435cf1dd68af77f64d55
etonnAi <93145207+etonnAi@users.noreply.github.com>
2021-11-25 16:07:32 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,48 @@
+function jaune () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+}
+function attendrejaune2 () {
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+function attendrejaune () {
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function Fermer () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
+function rouge () {
+    pins.digitalWritePin(DigitalPin.P2, 1)
+}
+function passerblanc () {
+    pins.digitalWritePin(DigitalPin.P16, 1)
+}
+function vert () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+}
+let temp = 0
+basic.forever(function () {
+    temp += 0.025
+    Fermer()
+    if (temp < 5) {
+        rouge()
+        if (temp > 4) {
+            if (temp * 5 % 1 < 0.5) {
+                attendrejaune()
+            }
+        } else {
+            passerblanc()
+        }
+    } else if (temp < 10) {
+        vert()
+        attendrejaune()
+    } else if (temp < 12) {
+        jaune()
+        attendrejaune()
+    } else {
+        temp = 0
+    }
+})
